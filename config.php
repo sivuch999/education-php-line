fe9cc1c0e32f6e507cd00c37f64410b6b6bc811c
sivuch999 <slowsoup@hotmail.com>
2020-11-25 16:17:34 +0700

    cleans

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     header('Content-Type: application/json');
 
-    $accessToken = "MjSQAoqTSBk2QK2lxVp8Wp0RKL4vexb8Yw7htqMlRq0orw0mtka+cOv7Q8mrfoa/qRJA+OyC4498AutLGkkUnEORNblSh9pfp8/KGrNv6mUDxYBwIvA+fgztQHxhqMXxcsRkXxZA15s3iiQXGDMVEgdB04t89/1O/w1cDnyilFU=";
+    $accessToken = "W3Q3hF0L1HX6+wK0FJEfNCNMEPi5+8NIXhYJmsa6EpsNWimxfPTeKyR8OI0Tl+UK7pRvvUNcQl/euu8xUBJnJPL6jXlamklI8luJg6U++fAaqreRCsXqpRPoS9r2lnkWqr9kbClqGVqUpmhlG0glZAdB04t89/1O/w1cDnyilFU=";
     $chennelSecret = "7c94b73ba15115a9170af251432a5e36";
 
     $contents = file_get_contents("php://input");
